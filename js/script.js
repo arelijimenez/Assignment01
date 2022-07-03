ada12ada5cfca8145718c0e4b8d6af65c565963d
Areli Jimenez <areli.jimenez.huerta@gmail.com>
2022-07-03 12:22:37 -0700

    Assignment01

---
+++ js/script.js
@@ -0,0 +1,118 @@
+// STEP 1
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction
+
+// STEP 2
+3.1416  //Numeric litera expression
+'Hi!'   //String literal expression
+true    // Boolean litera expression
+null    // Null literal expression
+
+// STEP 3 - Two examples or complex/ variable expressions
+let expression1 = (3+4) * 5;
+let expression2 = 15 % 4 + 9;
+
+// STEP 4
+let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou;
+
+// STEP 5
+let firstName = 'Areli';
+let lastName = "Jimenez";
+let zip = 91942;
+
+
+// STEP 6
+let age = 'Alexa is ' + 24;
+window.console.log(age);
+
+// STEP 7
+let isABoy = true + " Tom";
+window.console.log(isABoy);
+let isValid = 5 + true;
+window.console.log(isValid);
+
+// STEP 8
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+
+// STEP 9
+let nullVariable = null; 
+window.console.log(nullVariable);
+let firstName;
+window.console.log(firstName);
+
+// STEP 10
+   window.console.log(typeof '');
+   window.console.log(typeof NaN);
+   window.console.log(typeof false);
+   window.console.log(typeof [1,2,3]);
+   window.console.log(typeof undeclareVariable);
+
+// STEP 11 //
+window.alert("Hello "+"Areli Jimenez"+", welcome to the JavaScript class!");
+
+// STEP 12
+let name = 'Areli Jimenez';
+window.alert("Hello " + name + ", welcome to the JavaScript class !");
+
+// STEP 13
+let name = 'Areli Jimenez';
+let course = 'JavaScript';
+window.alert("Hello " + name + ", welcome to the " + course + " class !");
+
+//STEP 14
+let name = 'Areli Jimenez';
+let course = 'JavaScript';
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class !");
+
+// STEP 15
+let name = window.prompt("What is your name?");
+let course = 'JavaScript';
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class !");
+
+// STEP 16
+let name = window.prompt("What is your name?");
+let course = window.prompt("What is your class name?");
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class !");
+
+// STEP 17
+let x = 10;
+let y = 20;
+let z = x + y;
+window.console.log("x + y = " + z);
+
+// STEP 18
+let x = 20;
+x += 20;
+window.console.log("20 + 20 = " + x);
+
+// STEP 19
+let x = 20;
+x *= 5;
+window.console.log("20 * 5 = " + x);
+
+// STEP 20
+let x = 20 % 3;
+x /= 1;
+window.console.log("20 % 3 / 1 = " + x);
+
+// STEP 21
+let x = 10;
+let y = 30;
+window.console.log(y > x);
+
+// STEP 22
+let x = 10;
+let y = 30;
+window.console.log(y === x);
